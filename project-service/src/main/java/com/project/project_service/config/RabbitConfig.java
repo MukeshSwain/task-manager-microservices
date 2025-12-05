095ea920a4e2b6b17535fc38522ef1f683b3a337
Mukesh <mukeshswain2844@gmail.com>
2025-12-05 09:09:23 +0530

    rabbit and feign configured

---
+++ project-service/src/main/java/com/project/project_service/config/RabbitConfig.java
@@ -0,0 +1,30 @@
+package com.project.project_service.config;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+    public static final String EXCHANGE = "events.exchange";
+
+    // Routing keys for all email events
+    public static final String PROJECT_CREATED_KEY = "project.created";
+    public static final String PROJECT_MEMBER_ADDED_KEY = "project.member.added";
+
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter converter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(converter);
+        return rabbitTemplate;
+    }
+
+}
